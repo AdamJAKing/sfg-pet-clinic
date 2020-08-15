b96c6902fc3c71f034d7376b7fe3859d5c145ad5
Adam King <a.king.per@gmail.com>
2020-08-15 12:51:39 +0100

    Adding base entity. Closes #2

--- pet-clinic-data/src/main/java/dev/adamking/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/dev/adamking/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package dev.adamking.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/dev/adamking/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/dev/adamking/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package dev.adamking.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
