f5fd9656ab68f88f3c06846a6dcf6a0fc932adff
Adam King <a.king.per@gmail.com>
2020-08-18 21:51:33 +0100

    Multiple owners now save correctly. Closes #18

--- pet-clinic-web/src/main/java/dev/adamking/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dev/adamking/sfgpetclinic/bootstrap/DataLoader.java
@@ -31,9 +31,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1.getId(), owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona Glenanne");
-        owner1.setLastName("Weston");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2.getId(), owner2);
 
